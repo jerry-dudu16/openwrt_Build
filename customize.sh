0b7583fedc93b7915083e721e1ea6d63fb3ea788
jerry-dudu16 <59326257+jerry-dudu16@users.noreply.github.com>
2023-03-17 15:48:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,5 +7,5 @@ sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/conf
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' feeds/smpackage/.github/diy/default-settings
-sed -i '/kenzo/d' feeds/smpackage/.github/diy/default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/feeds/smpackage/.github/diy/default-settings
+sed -i '/kenzo/d' openwrt/feeds/smpackage/.github/diy/default-settings
